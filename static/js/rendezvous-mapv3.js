397a0faf924eb564758d6668b309b8c0223d488f
Hackbright Student <no-reply@hackbrightacademy.com>
2017-06-02 12:46:25 -0700

    fixed worst part of animated traffic alert so now it only happens twice. cleaned up js code.

--- static/js/rendezvous-mapv3.js
+++ static/js/rendezvous-mapv3.js
@@ -15,14 +15,14 @@ function initMap() {
       mapTypeId: 'roadmap',
       mapTypeControl: false,
       streetViewControl: false
-        });
+    });
 
     // add marker for destination point
     var marker = new google.maps.Marker({
     position: {'lat': center_lat, 'lng': center_lng },
     map: map,
     title: 'Rendezvous Here'
-        });
+    });
 
     //  make map dynamically resize when window is resized
     // google.maps.event.addDomListener(window, 'load', initialize);
@@ -31,145 +31,149 @@ function initMap() {
      google.maps.event.trigger(map, "resize");
      map.setCenter(center); 
     });
-  }
+}
 
 // this waits until the page is loaded, queries the database for waypoint data
 // for users on this invitation, then runs dataReceived
 $(function() {
     $.get('/map-data.json', { invite_id }, dataReceived);
-    });
+});
 
 //this is the callback function after json data is received.
 function dataReceived(results) {
     var allWaypoints = results['data'];
 
-        var symbolColors = ['blue', 'purple', 'red', 'yellow'];
-        var symbolColorCount = 0;  //for symbol color
-        var userLineColor;  //for route polyline
-        var iconColorCount = 0;  //for legend icon color
-        //iterate through waypoints_by_user to get users
-        for (var user in allWaypoints) {
-            //Save this user id for later
-            var thisUserId = allWaypoints[user]['id'];
-            if (thisUserId == user_id) { //this is the logged in user
-                var pickColor = 'white'; //white
-            } else {
-                pickColor = symbolColors[symbolColorCount];
-                symbolColorCount++;
-            }
+    var symbolColors = ['blue', 'purple', 'red', 'yellow'];
+    var symbolColorCount = 0;  //for symbol color
+    var userLineColor;  //for route polyline
+    var iconColorCount = 0;  //for legend icon color
+    //iterate through waypoints_by_user to get users
+    for (var user in allWaypoints) {
+        //Save this user id for later
+        var thisUserId = allWaypoints[user]['id'];
+        if (thisUserId == user_id) { //this is the logged in user
+            var pickColor = 'white'; //white
+        } else {
+            pickColor = symbolColors[symbolColorCount];
+            symbolColorCount++;
+        }
+     
+        // variable to hold the waypoints for this user
+        var pathListByUser = [];
+        //iterate through waypoints to get path for this user
+        for (var i=0; i<allWaypoints[user]['waypoints'].length; i++) {
+            pathListByUser.push({'lat': parseFloat(allWaypoints[user]['waypoints'][i][0]),
+                                 'lng': parseFloat(allWaypoints[user]['waypoints'][i][1])});
 
-         
-            // variable to hold the waypoints for this user
-            var pathListByUser = [];
+            //assign line color for logged in user
+            if (thisUserId == user_id) {
+                userLineColor = '#000000'; //black
+            } else {userLineColor = '#999999';} //gray
+      
+        //END loop for waypoints for this user
+        } 
 
-            //iterate through waypoints to get path for this user
-            for (var i=0; i<allWaypoints[user]['waypoints'].length; i++) {
-
-                  pathListByUser.push({'lat': parseFloat(allWaypoints[user]['waypoints'][i][0]),
-                                     'lng': parseFloat(allWaypoints[user]['waypoints'][i][1])});
-
-                  //assign line color for logged in user
-                  if (thisUserId == user_id) {
-                      userLineColor = '#000000'; //black
-                  } else {userLineColor = '#999999';} //gray
-              
-            //END loop for waypoints for this user
-              } 
-              var userLine = new google.maps.Polyline({
-                    path: pathListByUser,
-                    strokeColor: userLineColor,
-                    icons: [{
-                        icon: {
-                            path: google.maps.SymbolPath.CIRCLE,
-                            scale: 4,
-                            strokeColor: pickColor
-                            },
-                        offset: '0%'
-                      }],
-                     map: map
-                      });
-            //save user data so I can update icon position/legend in realitme later
+        //Add line to the map
+//This should probably be moved out of the
+//loop so it does it ONCE when pathListByUser is complete for each user
+    var userLine = new google.maps.Polyline({
+        path: pathListByUser,
+        strokeColor: userLineColor,
+        icons: [{
+            icon: {
+                path: google.maps.SymbolPath.CIRCLE,
+                scale: 4,
+                strokeColor: pickColor
+                },
+            offset: '0%'
+        }],
+        map: map
+    });
+    //save user data so I can update icon position/legend in realitme later
     //format: {user_id: {'line': googlemap line object, 'name': name,
-        //    'eta_text': eta_text, 'eta_value': eta_value, 'symbolColor': color}} 
-            userLines[thisUserId]= {'line': userLine, 'name': allWaypoints[user]['name'],
-           'starting_eta_text': allWaypoints[user]['starting_eta_text'],
-           'starting_eta_value': allWaypoints[user]['starting_eta_value'],
-           'symbolColor': pickColor}
+    //    'eta_text': eta_text, 'eta_value': eta_value, 'symbolColor': color}} 
+    userLines[thisUserId]= {
+        'line': userLine, 'name': allWaypoints[user]['name'],
+        'starting_eta_text': allWaypoints[user]['starting_eta_text'],
+        'starting_eta_value': allWaypoints[user]['starting_eta_value'],
+        'symbolColor': pickColor
+    }
 
-        //legend
-            var iconBase = 'https://maps.google.com/mapfiles/kml/paddle/';
-            var icons = {
-              thisUserId: {
-                name: userLines[thisUserId]['name'] + " - " +userLines[thisUserId]['starting_eta_text'],
-                icon: iconBase + changeColorName(userLines[thisUserId]['symbolColor']) +'-circle-lv.png'
-                }
-            };
-            var legend = document.getElementById('legend');
-            for (var key in icons) {
-                var type = icons[key];
-                var name = type.name;
-                var icon = type.icon;
-                var div = document.createElement('div');
-                div.innerHTML = '<img src="' + icon + '"> ' + name;
-                legend.appendChild(div);
-              }
-            map.controls[google.maps.ControlPosition.TOP_RIGHT].push(legend);
-        //END loop for users in allWaypoints
-          }
+    //legend
+    var iconBase = 'https://maps.google.com/mapfiles/kml/paddle/';
+    var icons = {
+        thisUserId: {
+            name: userLines[thisUserId]['name'] + " - " +userLines[thisUserId]['starting_eta_text'],
+            icon: iconBase + changeColorName(userLines[thisUserId]['symbolColor']) +'-circle-lv.png'
+        }
+    };
+    var legend = document.getElementById('legend');
+    for (var key in icons) {
+        var type = icons[key];
+        var name = type.name;
+        var icon = type.icon;
+        var div = document.createElement('div');
+        div.innerHTML = '<img src="' + icon + '"> ' + name;
+        legend.appendChild(div);
+    }
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(legend);
+    //END loop for users in allWaypoints
+    }
 
     //animate the symbols on the lines
     var lineSpeed = 40;
-    for(var user in userLines)
-    { 
-    animateSymbol(userLines[user]['line'], lineSpeed, userLines[thisUserId]['name']);
-    lineSpeed = lineSpeed + 40;
+    for(var user in userLines){
+        animateSymbol(userLines[user]['line'], lineSpeed, userLines[user]['name']);
+        lineSpeed = lineSpeed + 40;
     }
-
 //END of dataReceived function
 }
 
-// Use the DOM setInterval() function to change the offset of the symbol
-// at fixed intervals.  offset 0=the begining of the polyline, 100=the end
+// Animate the symbol. Offset 0=the begining of the polyline, 100=the end.
 function animateSymbol(inputLine, inputSpeed, userName) {
-    
     var id1;
     var count1 = 0;
     var line = inputLine;
     var speed = inputSpeed;
 
     id1 = setInterval(function() {
-      count1 = (count1 + 1);
-      var icons = line.get('icons');
-      icons[0].offset = (count1 / 2) + '%';
-      line.set('icons', icons);
-    // make traffic alert for user3 not working in here, try elsewhere
-    if ((userName == "Test User 3") && (parseInt(line.get('icons')[0].offset) == 80)) {
-        alert ("Test User 3 has encountered traffic. 4 minute delay.")
-    }
-    // make animation to stop at destination
-      if (parseInt(line.get('icons')[0].offset) > 99.5) {     
+        count1 = (count1 + 1);
+        var icons = line.get('icons');
+        icons[0].offset = (count1 / 2) + '%';
+        line.set('icons', icons);
+
+        // make traffic alert for user3 not working in here, try elsewhere
+        if ((userName == "Test User 3") && (parseInt(line.get('icons')[0].offset) == 80)) {
+            trafficAlert(userName);
+        }
+        // make animation to stop at destination
+        if (parseInt(line.get('icons')[0].offset) > 99.5) {     
             icons[0].offset = '100%';  
             clearInterval(id1);
             // remove polyline from map when arrive
             line.setMap(null);          
             alert(userName + 'arrived!')
-    }
-     }, speed);
-    
+            // make a list of "active users" and delete from that list
+            // when this happens and then call function that re-renders
+            // legend based on users that are in the list?
+        }
+    }, speed);
+// END animateSymbol
 }
 
-function trafficAlert () {
-    alert (userName + " has encountered traffic. 4 minute delay.")
+function trafficAlert (userName) {
+    name = userName;
+    alert (name + " has encountered traffic. 4 minute delay.");
 }
 
 function changeColorName (color) {
-  var colors = {
-    'white': 'wht',
-    'blue': 'blu',
-    'purple': 'purple',
-    'red': 'red',
-    'yellow': 'ylw',
-    'default': 'blu'
-  };
-  return (colors[color] || colors['default']);
+    var colors = {
+        'white': 'wht',
+        'blue': 'blu',
+        'purple': 'purple',
+        'red': 'red',
+        'yellow': 'ylw',
+        'default': 'blu'
+    };
+    return (colors[color] || colors['default']);
 }
